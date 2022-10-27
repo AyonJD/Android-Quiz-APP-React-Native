33e1bbfbd61792354ba5e445ba4da779b0fb3351
Ayon Jodder <ayonjodder177@gmail.com>
2022-10-27 19:00:32 +0600

    Home page initial design

--- Src/Screens/HomeScreen.js
+++ Src/Screens/HomeScreen.js
@@ -1,23 +1,54 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
-import { AuthContext } from '../../App';
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-const HomeScreen = () => {
-    const { token, user, logoutToken } = React.useContext(AuthContext);
-    console.log(token, user)
+const HomeScreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
-            <Text style={styles.welcomeHeader}>Welcome</Text>
-            <Button title='Sign Out' onPress={logoutToken} />
+            <Text>Quizze</Text>
+            <View style={styles.bannerContainer}>
+                <Image
+                    source={require('../../assets/quiz_home.png')}
+                    style={styles.banner}
+                    resizeMode="contain"
+                />
+            </View>
+            <TouchableOpacity
+                onPress={() => navigation.navigate('QuizScreen')}
+                style={styles.button}>
+                <Text style={styles.buttonText}>Start</Text>
+            </TouchableOpacity>
         </View>
-    )
-}
+    );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#009387'
+    banner: {
+        height: 300,
+        width: 300,
     },
-})
+    bannerContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+    },
+    container: {
+        paddingTop: 40,
+        paddingHorizontal: 20,
+        height: '100%',
+    },
+    button: {
+        width: '100%',
+        backgroundColor: '#009387',
+        padding: 10,
+        borderRadius: 16,
+        alignItems: 'center',
+        marginBottom: 30,
+    },
+    buttonText: {
+        fontSize: 20,
+        fontWeight: '600',
+        color: 'white',
+    },
+});
