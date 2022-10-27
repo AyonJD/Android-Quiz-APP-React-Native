40093f29dbd20ceebf8da67db47d2bb762eb2d51
Ayon Jodder <ayonjodder177@gmail.com>
2022-10-27 19:05:08 +0600

    Initial quiz

--- Src/Components/Navigation/ScreenNavigation.js
+++ Src/Components/Navigation/ScreenNavigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../../Screens/HomeScreen';
+import QuizScreen from '../../Screens/QuizScreen';
 
 
 
@@ -8,8 +9,9 @@ const Stack = createNativeStackNavigator();
 
 const ScreenNavigation = () => {
     return (
-        <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="QuizScreen" component={QuizScreen} />
         </Stack.Navigator>
     )
 }
